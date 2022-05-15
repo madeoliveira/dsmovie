3476f85e48822871d68cd2fe1a3e2f7353fdb3b4
Your Name <you@example.com>
2022-05-14 23:42:14 -0300

    Show score

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -25,11 +25,11 @@ function Listing() {
     );
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`).then(response => {
-            const data = response.data as MoviePage;
-            setPage(data);
-        });
-
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
+            .then(response => {
+                const data = response.data as MoviePage;
+                setPage(data);
+            });
     }, [pageNumber]);
     return (
         <>
@@ -38,7 +38,7 @@ function Listing() {
                 <div className="row">
 
                     {page.content.map(movie => (
-                        <div key ={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                             <MovieCard movie={movie} />
                         </div>
                     )
