d485a7974263aee245f9a15e4d56f37e859c872f
Your Name <you@example.com>
2022-05-15 00:15:14 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
-import Pagination from "components/Navbar/Pagination";
+import Pagination from "components/Pagination";
 import { useEffect, useState } from "react";
 import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
@@ -31,9 +31,13 @@ function Listing() {
                 setPage(data);
             });
     }, [pageNumber]);
+    const handlePageChange = (newPageNumber: number) => {
+        setPageNumber(newPageNumber);
+    }
+
     return (
         <>
-            <Pagination />
+            <Pagination page={page} onChange={handlePageChange} />
             <div className="container">
                 <div className="row">
 
